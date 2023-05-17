86bbd7c71086aec424ec693eecd0c6903d7d46cb
António Pereira <antonio_m_sp@hotmail.com>
2023-05-17 23:45:42 +0100

    update gecko driver, clean up code.

--- stats_scraper.py
+++ stats_scraper.py
@@ -18,7 +18,7 @@ from openpyxl import load_workbook
 """
 HLL Stats Scraper
 
-The Script goes through several private and public RCONS from game Hell Let Loose
+The Script goes through several private and public RCONS from the game Hell Let Loose
 and retrieves data from best players performance in several matches aggrouping it 
 in a .xlsx file in the end.
 
@@ -30,12 +30,9 @@ To debug code remove headless line to see normal script run.
 # ARGUMENTS
 #KILLS_PER_MINUTE_REQ = 0.8
 KILLS_REQ = 65
-GAME_ID_SEARCH_NUMBER = 8500
+GAME_ID_SEARCH_NUMBER = 10
 
 ####################################################################
-
-
-
 
 HLL_PRIVATE_RCONS = {
   #"EXD": "http://164.90.235.39:8010/#/gamescoreboard/", rcon changed
@@ -53,7 +50,6 @@ HLL_PRIVATE_RCONS = {
 HLL_PUBLIC_RCONS = {
   #"82AD_2_test": "https://server2.82nd.gg/#/gamescoreboard/25982",
   #"82AD_1": "https://server1.82nd.gg/#/gamescoreboard/"
-  
   #"82AD_3": "https://server3.82nd.gg/#/gamescoreboard/", # event server
   #"HLL_PL_1": "https://stats1.hellletloose.pl/#/gamescoreboard/",
   "82AD": "https://server2.82nd.gg/#/gamescoreboard/", #82AD_2
@@ -93,7 +89,7 @@ def parse_table_data():
 
     df = df.astype({'Kill(s) / minute':'float'})
     df = df.astype({'Kills':'int'})
-    #df = df.loc[(df['Kill(s) / minute'] >= KILLS_PER_MINUTE_REQ) & (df['Kills'] >= KILLS_REQ)]
+    # df = df.loc[(df['Kill(s) / minute'] >= KILLS_PER_MINUTE_REQ) & (df['Kills'] >= KILLS_REQ)]
     df = df.loc[df['Kills'] >= KILLS_REQ]
 
     return df
@@ -108,7 +104,7 @@ def click_more_stats():
     more_stats.click()
 
 def get_table_data_from_pub_server():
-    #kill_sort = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[17]/div/div[2]/div/div[3]/table/thead/tr/th[2]")
+    # kill_sort = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[17]/div/div[2]/div/div[3]/table/thead/tr/th[2]")
     try:
         kill_sort = WebDriverWait(driver,3).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/div/div[3]/div[17]/div/div[2]/div/div[3]/table/thead/tr/th[2]")))
         kill_sort.click() 
@@ -131,12 +127,12 @@ def create_excel():
         filename = name[0].split('/')
         filename = filename[-1]
 
-        #create xlsx
+        # Create .xlsx
         if first_entry:
             with pd.ExcelWriter('hll_stats.xlsx', mode='w') as writer:  
                 df.to_excel(writer, sheet_name=filename, index=False)
 
-        #append    
+        # Append    
         else:
             excel_path = "hll_stats.xlsx"
             ExcelWorkbook = load_workbook(excel_path)
@@ -171,9 +167,6 @@ def load_private_rcons():
         current_game_id = driver.current_url.rpartition('/')[2]
         game_url_default = re.sub(r'\d+$', '', driver.current_url)
         
-
-        
-
         try:
             current_game_id = int(current_game_id)
             click_more_stats()
@@ -216,19 +209,15 @@ def load_public_rcons():
         if first_entry_header:
             time.sleep(3)
         
-
         current_game_id = driver.current_url.rpartition('/')[2]
         game_url_default = re.sub(r'\d+$', '', driver.current_url)
         
-        
-
         try:
             current_game_id = int(current_game_id)
             click_more_stats()
         except:
             print("No stats for this link: ", server_name + " - " + driver.current_url)
 
-        
         for i in range(GAME_ID_SEARCH_NUMBER):
             print("Public RCON: " + server_name + " - " + game_url_default + str(current_game_id-i))
             try:
@@ -239,7 +228,7 @@ def load_public_rcons():
                     driver.get(game_url_default + str(int(current_game_id-i)))
 
                 time.sleep(1)
-                #driver.maximize_window()
+                # driver.maximize_window()
                 get_table_data_from_pub_server()
                 df = parse_table_data()
 
@@ -269,7 +258,7 @@ if __name__ == "__main__":
 
     driver = init_webdriver()
 
-    load_private_rcons()
+    # load_private_rcons()
     load_public_rcons()
 
     exit_functions()
